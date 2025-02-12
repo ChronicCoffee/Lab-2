f02cb195a4f4cbea8ae40cff9f7200f7f0ac3dda
Asfand7867 <161638610+Asfand7867@users.noreply.github.com>
2025-02-12 12:09:35 -0700

    changes to student class

--- Lab2/src/exercise1/Student.java
+++ Lab2/src/exercise1/Student.java
@@ -7,7 +7,23 @@ public class Student
 	
 	public Student( String name, int age )
 	{
-		this.name = name;
+		this.setName(name);
+		this.setAge(age);
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
